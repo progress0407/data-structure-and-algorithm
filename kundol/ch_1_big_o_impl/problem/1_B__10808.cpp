cb167a85a3e5dbb7ee803860b85d8a035215072e
progress0407 <progress0407@gmail.com>
2023-07-11 23:26:29 +0900

    1-B 10808 알파벳 개수 풀이

---
+++ kundol/ch_1_big_o_impl/problem/1_B__10808.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * 알파벳 개수
+ */
+
+string input;
+int a[26];
+
+int main() {
+
+    cin >>input;
+
+    for(auto s: input) {
+        int i = s - 97;
+        a[i] ++;
+    }
+
+    for(int e: a) cout << e << " ";
+
+    return 0;
+}
+
+
