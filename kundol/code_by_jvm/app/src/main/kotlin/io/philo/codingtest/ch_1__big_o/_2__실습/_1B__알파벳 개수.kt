3cb179487bd6a2bedbad73b76671e1d5aef81979
progress0407 <progress0407@gmail.com>
2023-12-19 01:40:40 +0900

    1-B 풀이

---
+++ kundol/code_by_jvm/app/src/main/kotlin/io/philo/codingtest/ch_1__big_o/_2__실습/_1B__알파벳 개수.kt
@@ -0,0 +1,20 @@
+package io.philo.codingtest.ch_1__big_o._2__실습
+
+fun main(args: Array<String>)  {
+
+    val input = readln().map { it.toString() }.toList()
+    val cntArr = Array<Int>(26) { 0 }
+
+    for (alphabet in input) {
+        val index = alphabet.toNum()
+        cntArr[index] ++
+    }
+
+    val result = cntArr.joinToString(separator = " ")
+
+    println(result)
+}
+
+private fun String.toNum(): Int {
+    return this[0].code - 'a'.code
+}
